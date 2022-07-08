cab8f7d45440561bee6668fa6510df797dedcfe1
usmanferoz <usman_feroz786@hotmail.com>
2022-07-08 20:11:25 +0500

    landing page

--- src/components/home.js
+++ src/components/home.js
@@ -8,17 +8,29 @@ import PersonIcon from '@mui/icons-material/Person';
 import MenuIcon from '@mui/icons-material/Menu';
 import leafs from '../static/leafs.png';
 import birds from '../static/birds.png';
+import { width } from "@mui/system";
+import intro from '../static/intro.mp4';
 
 
 function Home() {
-    const [isMobile, setIsMobile] = useState(false);
+    const [isTopNav, setIsTopNav] = useState(false);
+    const [isLeftNav, setIsLeftNav] = useState(false)
 
-    const ontoggle = () => {
-        if (isMobile === false) {
-            setIsMobile(true);
+    const toogleTop = () => {
+        if (isTopNav === false) {
+            setIsTopNav(true);
         }
         else {
-            setIsMobile(false);
+            setIsTopNav(false);
+        }
+    }
+
+    const toogleLeft = () => {
+        if (isLeftNav === false) {
+            setIsLeftNav(true);
+        }
+        else {
+            setIsLeftNav(false);
         }
     }
     return (
@@ -28,10 +40,10 @@ function Home() {
                     <img src={logo}></img>
                 </div>
                 <div className="links">
-                    <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
+                    <ul className={isTopNav ? "nav-links-mobile" : "nav-links"}>
                         <li className="usericon"><PersonIcon style={{ marginTop: "25px", fontSize: "2em" }} /></li>
                         <li>
-                            <p className="signin">SING IN</p>
+                            <p className="signin">SIGN IN</p>
                         </li>
                         <li><p>SIGN UP</p></li>
                         <li><input type="text" placeholder="Search.." className="search"></input></li>
@@ -41,8 +53,8 @@ function Home() {
 
                     <div className="menu">
                         <button style={{ backgroundColor: "transparent", border: "none" }}
-                            onClick={() => { ontoggle(); }}>
-                            <MenuIcon sx={{ fontSize: 40 , color:"white"}} /> </button>
+                            onClick={() => { toogleTop(); }}>
+                            <MenuIcon sx={{ fontSize: 40, color: "white" }} /> </button>
                     </div>
                 </div>
             </nav>
@@ -50,19 +62,12 @@ function Home() {
             <main>
                 <div className="left-sidebar">
                     <div style={{ height: "2px" }}></div>
-
-                    <div className="left-menu">
-                        <button style={{ backgroundColor: "transparent", border: "none" }}
-                            onClick={() => { ontoggle(); }}>
-                            <MenuIcon sx={{ fontSize: 40 , color:"white"}} /> </button>
-                    </div>
-
-                    <ul className="left-list">
-                        <li> Clinical Trial Finder</li>
-                        <li> CC Treatment Guide</li>
-                        <li> Pharmaceutical Consulting</li>
-                        <li> Cancer Center Consulting</li>
-                        <li> My Profile</li>
+                    <ul className={isLeftNav ? "mobile-left-list" : "left-list"}>
+                        <li> <div className="dot1"></div> Clinical Trial Finder</li>
+                        <li><div className="dot2"></div> CC Treatment Guide</li>
+                        <li><div className="dot3"></div> Pharmaceutical Consulting</li>
+                        <li><div className="dot4"></div> Cancer Center Consulting</li>
+                        <li><div className="dot5"></div> My Profile</li>
 
                         <div className="sister-sites">
                             <p className="tag">SISTER SITES</p>
@@ -77,15 +82,31 @@ function Home() {
                         </div>
 
                     </ul>
+                    <div className="left-menu">
+                        <button style={{ backgroundColor: "transparent", border: "none" }}
+                            onClick={() => { toogleLeft(); }}>
+                            <MenuIcon sx={{ fontSize: 40, color: "black" }} /> </button>
+                    </div>
                 </div>
-                <div className="right-content"></div>
+                <div className="right-content">
+                    <div className="right-container">
+                        <div className="text">
+                            <h1>Welcome to Cancer Clarity</h1>
+                            <p>Choosing the right disease process for your asset is imperative. Several drugs have performed suboptimally because they weren't studied in the ideal disease. As opposed to all of our competitors, we have broad expertise in all of hematology and medical oncology, uniquely enabling us to best identify where to position your asset. Our consultants all have MD and MBA degrees, and have conducted clinical trials in numerous hematologic and oncologist disorders. Our CEO has an MD, PhD, and MBA, and has participated in nearly 100 clinical trials. He has three papers in the NEJM and two in Lancet. He served as the medical director for Xbiotech, and was the national medical monitor for three of their trials.</p>
+                        </div>
+                        <div className="media">
+                            <video src={intro} width="750" height="500" autoPlay loop muted/>
+                        
+                        </div>
+                    </div>
+                </div>
             </main>
             <footer>
                 <div className="footer">
                     <img className="birds" src={birds}></img>
                 </div>
                 <div className="footer">
-                    <img src={leafs} ></img>
+                    <img className="plant" src={leafs} ></img>
                 </div>
             </footer>
         </>
